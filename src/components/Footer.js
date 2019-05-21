a24daf897f1eec1fc64a9576400edfb20265daff
Iain Henderson <iainhenderson@mac.com>
2019-05-21 17:57:47 +0100

    amend twitter handle
    amend twitter handle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
-                    <li><a  target="_blank" href="https://twitter.com/MyDataScotland" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                    <li><a  target="_blank" href="https://twitter.com/dids4kids" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                   <li><a  target="_blank" href="https://mydata.org" className="icon alt fa-globe"><span className="label">Website</span></a></li>
 
                   <li><a href="mailto:scotland@mydata.org" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
